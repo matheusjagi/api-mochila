cb1fcf8eaf3e38dc020a0d3c50042418e861ba50
Matheus Jagi <jagi.matheus@gmail.com>
2021-05-24 19:10:17 -0300

    subindo o projeto

---
+++ src/main/java/com/ifes/algoritmogenetico/apimochila/ApiMochilaApplication.java
@@ -0,0 +1,13 @@
+package com.ifes.algoritmogenetico.apimochila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMochilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMochilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifes/algoritmogenetico/apimochila/ApiMochilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifes.algoritmogenetico.apimochila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMochilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
